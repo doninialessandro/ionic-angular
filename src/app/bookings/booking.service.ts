d783a7b3543988d54d4199e0e8264e828439f352
Alessandro Donini <alessandro.donini.6@gmail.com>
2020-06-14 11:52:53 +0200

    feat(booking.service.ts): create and delete bookings

--- src/app/bookings/booking.service.ts
+++ src/app/bookings/booking.service.ts
@@ -1,9 +1,24 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 import { BehaviorSubject } from 'rxjs';
-import { take, tap, delay } from 'rxjs/operators';
+import { take, tap, delay, switchMap, map } from 'rxjs/operators';
 
 import { Booking } from './booking.model';
 import { AuthService } from '../auth/auth.service';
+
+import { environment } from '../../environments/environment';
+
+interface BookingData {
+  bookedFrom: string;
+  bookedTo: string;
+  firstName: string;
+  guestNumber: number;
+  lastName: string;
+  placeId: string;
+  placeImage: string;
+  placeTitle: string;
+  userId: string;
+}
 
 @Injectable({ providedIn: 'root' })
 export class BookingService {
@@ -13,7 +28,7 @@ export class BookingService {
     return this._bookings.asObservable();
   }
 
-  constructor(private authService: AuthService) {}
+  constructor(private authService: AuthService, private http: HttpClient) {}
 
   addBooking(
     placeId: string,
@@ -25,6 +40,7 @@ export class BookingService {
     dateFrom: Date,
     dateTo: Date
   ) {
+    let generatedId: string;
     const newBooking = new Booking(
       Math.random().toString(),
       placeId,
@@ -37,22 +53,69 @@ export class BookingService {
       dateFrom,
       dateTo
     );
-    return this.bookings.pipe(
-      take(1),
-      delay(1000),
-      tap((bookings) => {
-        this._bookings.next(bookings.concat(newBooking));
+    return this.http
+      .post<{ name: string }>(`${environment.apiUrl}/bookings.json`, {
+        ...newBooking,
+        id: null,
       })
-    );
+      .pipe(
+        switchMap((resData) => {
+          generatedId = resData.name;
+          return this.bookings;
+        }),
+        take(1),
+        tap((bookings) => {
+          newBooking.id = generatedId;
+          this._bookings.next(bookings.concat(newBooking));
+        })
+      );
   }
 
   cancelBooking(bookingId: string) {
-    return this.bookings.pipe(
-      take(1),
-      delay(1000),
-      tap((bookings) => {
-        this._bookings.next(bookings.filter((b) => b.id !== bookingId));
-      })
-    );
+    return this.http
+      .delete(`${environment.apiUrl}/bookings/${bookingId}.json`)
+      .pipe(
+        switchMap(() => {
+          return this.bookings;
+        }),
+        take(1),
+        tap((bookings) => {
+          this._bookings.next(bookings.filter((b) => b.id !== bookingId));
+        })
+      );
+  }
+
+  fetchBookings() {
+    return this.http
+      .get<{ [key: string]: BookingData }>(
+        `${environment.apiUrl}/bookings.json?orderBy="userId"&equalTo="${this.authService.userId}"`
+      )
+      .pipe(
+        map((bookingData) => {
+          const bookings = [];
+          for (const key in bookingData) {
+            if (bookingData.hasOwnProperty(key)) {
+              bookings.push(
+                new Booking(
+                  key,
+                  bookingData[key].placeId,
+                  bookingData[key].userId,
+                  bookingData[key].placeTitle,
+                  bookingData[key].placeImage,
+                  bookingData[key].firstName,
+                  bookingData[key].lastName,
+                  bookingData[key].guestNumber,
+                  new Date(bookingData[key].bookedFrom),
+                  new Date(bookingData[key].bookedTo)
+                )
+              );
+            }
+          }
+          return bookings;
+        }),
+        tap((bookings) => {
+          this._bookings.next(bookings);
+        })
+      );
   }
 }
